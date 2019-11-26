88bee5f8c3e362a99a1202d42a6d5e39a56490f7
strantr <strantr@users.noreply.github.com>
2019-11-26 21:29:29 +0000

    update

---
+++ routes.js
@@ -0,0 +1,45 @@
+var routes = [
+  { name: "#AAAAAAA-SimonTest", grade: "V0", points: [0, 7, 7, 20, 21, 38] },
+  { name: "Silly Boy", grade: "V4", points: [7, 20, 21, 38] },
+  { name: "Silly Boy SDS", grade: "V5", points: [1, 20, 21, 38] },
+  { name: "The Pex Dyno", grade: "V7", points: [18, 18] },
+  { name: "Stretch", grade: "V4", points: [2, 2, 11, 20, 27] },
+  { name: "Vitalite", grade: "V6", points: [3, 3, 16, 21, 22] },
+  { name: "The Fat Neck", grade: "V1", points: [8, 8, 42, 37] },
+  { name: "Big Eric", grade: "V6", points: [40, 40, 50, 47] },
+  { name: "Andy Who?", grade: "V7", points: [48, 49, 50] },
+  { name: "Punks at Pex", grade: "V6", points: [49, 66, 51] },
+  { name: "Silly Boy Direct", grade: "V5", points: [7, 20, 21] },
+  { name: "Red Flash", grade: "V5", points: [6, 7] },
+  { name: "Big Nick", grade: "V6", points: [5, 12, 22, 32, 33, 47] },
+  { name: "The South West Overhang", grade: "V8", points: [54, 56, 55] },
+  {
+    name: "The South West Overhang SDS",
+    grade: "V9",
+    points: [58, 58, 54, 56, 55]
+  },
+  { name: "Trad Sit Down", grade: "V1", points: [58, 60, 57] },
+  { name: "North West Overhang", grade: "V8", points: [61, 62, 57, 65] },
+  {
+    name: "North West Overhang SDS",
+    grade: "V10",
+    points: [58, 61, 62, 57, 65]
+  },
+  { name: "Red Al", grade: "V3", points: [55, 66, 25, 27] },
+  { name: "Blue Jim", grade: "V2", points: [7, 7, 21, 27] },
+  { name: "Twister", grade: "V3", points: [2, 2, 8, 34] },
+  { name: "Get Better", grade: "V4", points: [2, 2, 13, 32, 34] },
+  { name: "Bog Standard", grade: "V3", points: [51, 66, 55] },
+  { name: "Poor Leno", grade: "V3", points: [1, 1, 5, 26] },
+  {
+    name: "Dimples Traverse Short",
+    grade: "V3",
+    points: [9, 10, 67, 42, 44, 46]
+  },
+  { name: "Yer Basic", grade: "V2", points: [5, 66, 24, 27] },
+  { name: "1066", grade: "V1", points: [7, 21, 26, 27] },
+  { name: "King Kenny", grade: "V6", points: [48, 49, 51] },
+  { name: "Idiotic Man SDS", grade: "V7", points: [2, 2, 7, 21] },
+  { name: "Vitaliter", grade: "V7", points: [3, 3, 16, 21] },
+  { name: "Small Snick SDS", grade: "V5", points: [2, 2, 13, 38] }
+];
